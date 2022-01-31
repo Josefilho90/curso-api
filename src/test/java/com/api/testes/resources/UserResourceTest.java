b33e79d66249ffb524e79e080e50d8f062d4aa02
Jose Filho <jsflh7@gmail.com>
2022-01-31 16:44:02 -0300

    testes para endpoint fiNDaLL da classe user resource implementado

--- src/test/java/com/api/testes/resources/UserResourceTest.java
+++ src/test/java/com/api/testes/resources/UserResourceTest.java
@@ -3,21 +3,21 @@ package com.api.testes.resources;
 import com.api.testes.domain.User;
 import com.api.testes.domain.dto.UserDTO;
 import com.api.testes.services.impl.UserServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
@@ -29,6 +29,7 @@ class UserResourceTest {
     private static final String NAME = "Jose";
     private static final String EMAIL = "Jose@gmail.com";
     private static final String PASSWORD = "123";
+    private static final Integer INDEX = 0;
 
     private User user;
     private UserDTO userDTO;
@@ -50,7 +51,7 @@ class UserResourceTest {
     }
 
     @Test
-    void WhenFindyByIdThenReturnSuccess() {
+    void WhenFindByIdThenReturnSuccess() {
 
         when(service.findyById(anyInt())).thenReturn(user);
         when(mapper.map(any(), any())).thenReturn(userDTO);
@@ -67,13 +68,26 @@ class UserResourceTest {
         assertEquals(EMAIL, response.getBody().getEmail());
         assertEquals(PASSWORD, response.getBody().getPassword());
 
-
-
-
     }
 
     @Test
-    void findAll() {
+    void WhenFindAllThenReturnListOfUserDTO() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+
     }
 
     @Test

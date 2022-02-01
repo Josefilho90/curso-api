5dfb8a050a656b79532d841f646c37a8511c1866
Jose Filho <jsflh7@gmail.com>
2022-02-01 17:58:22 -0300

    revertendo error api teste aplication

--- src/main/java/com/api/testes/ApiTesteApplication.java
+++ src/main/java/com/api/testes/ApiTesteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiTesteApplication {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(ApiTesteApplication.class, args);
 		
 	}
 	
